87a371475a138a80625dcbcc2713f0ea6ddc4e81
Leslie Cuello <lesliecuellocatalan@gmail.com>
2019-03-27 16:21:25 -0300

    Update run-docker.sh

--- jenkins/scripts/run-docker.sh
+++ jenkins/scripts/run-docker.sh
@@ -1,1 +1,7 @@
+#!/usr/bin/env sh
 
+echo 'The following command run docker container'
+
+docker inspect -f . node:6-alpine
+docker pull node:6-alpine
+docker run -p 3000:3000 --name node-server node:6-alpine
