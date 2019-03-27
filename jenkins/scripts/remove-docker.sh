5a927f81605f125a0204732624c198af9ad1688b
Leslie Cuello Catalan <lesliecuellocatalan@@gmail.com>
2019-03-27 17:14:51 -0300

    Add remove container

--- jenkins/scripts/remove-docker.sh
+++ jenkins/scripts/remove-docker.sh
@@ -2,4 +2,5 @@
 
 echo 'The following command delete docker previous container'
 
-docker ps -f name=node-server -q | xargs --no-run-if-empty docker container rm -f
+docker ps -f name=node-server -q | xargs --no-run-if-empty docker container stop
+docker container ls -a -f name=node-server -q | xargs -r docker container rm
